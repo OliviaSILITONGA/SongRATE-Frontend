7c0ab709343ae5d33841c055f1375326e6f92f01
Olivia Gabriella Silitonga <oliviasilitonga03@gmail.com>
2025-11-21 10:39:42 +0700

    Delete SongRATE directory

--- SongRATE/src/components/AlbumCard.jsx
+++
@@ -1,8 +0,0 @@
-export default function AlbumCard({ title, image }) {
-  return (
-    <div className="bg-[#1c1c1c] p-4 rounded-xl text-white w-56">
-      <img src={image} alt={title} className="rounded-xl mb-3" />
-      <h3 className="text-lg font-semibold">{title}</h3>
-    </div>
-  );
-}
