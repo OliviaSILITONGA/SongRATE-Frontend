7819d31d10000913e5dc9a0c304686465ed965ae
Olivia Gabriella Silitonga <oliviasilitonga03@gmail.com>
2025-11-21 09:40:06 +0700

    Delete Logo.jsx

--- Logo.jsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-  return (
-    <nav className="w-full px-10 py-6 flex justify-between items-center text-white">
-      <h1 className="text-3xl font-bold tracking-wide">SONGRATE</h1>
-    </nav>
-  );
-}
