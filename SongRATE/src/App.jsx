1117f02d26b595615f732b3b2c5e87cf1bc5f0ea
Olivia Gabriella Silitonga <oliviasilitonga03@gmail.com>
2025-11-21 10:07:20 +0700

    Initial commit

---
+++ SongRATE/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import ForgotPassword from "./pages/ForgotPassword";
+import CreatePassword from "./pages/CreatePassword";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/create-password" element={<CreatePassword />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
